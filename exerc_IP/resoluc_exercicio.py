f9843337269d07ba6d8db34ba8cefa84308fec56
Matheus Rodrigues <67073149+mtheusrd@users.noreply.github.com>
2020-09-02 19:20:18 -0300

    Exercicio - Arquivos
    Exercicio com arquivos.

---
+++ exerc_IP/resoluc_exercicio.py
@@ -0,0 +1,36 @@
+
+def validaIP(ipAddress):
+    blocos = ipAddress.split('.')
+    if (len(blocos) != 4):
+        return False
+    for i in blocos:
+        i_i = int(i)
+        if (i_i < 0) or (i_i > 255):
+            return False
+    return True
+
+# FLUXO PRINCIPAL
+arquivoEntrada = open('vlr_entrada.txt', 'r')
+linhas = arquivoEntrada.readlines()
+arquivoEntrada.close()
+
+ipsValidos = []
+ipsInvalidos = []
+
+for ip in linhas:
+    if (validaIP(ip.strip())):
+        ipsValidos.append(ip.strip())
+    else:
+        ipsInvalidos.append(ip.strip())
+
+arquivoSaida = open('vlr_saida.txt', 'w')
+
+arquivoSaida.write('[IPs Validos]\n')
+for ip in ipsValidos:
+    arquivoSaida.write('%s\n' % ip)
+
+arquivoSaida.write('\n[IPs Invalidos]\n')
+for ip in ipsInvalidos:
+    arquivoSaida.write('%s\n' % ip)
+
+arquivoSaida.close()
